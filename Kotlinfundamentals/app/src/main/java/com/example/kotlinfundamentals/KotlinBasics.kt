7e713e48a1a64ff028b63d0326e2106d16a00636
Daniel <daniel.ghebrat@a115.bg>
2022-03-16 18:08:03 +0000

    initial commit

---
+++ Kotlinfundamentals/app/src/main/java/com/example/kotlinfundamentals/KotlinBasics.kt
@@ -0,0 +1,60 @@
+package com.example.kotlinfundamentals
+
+import kotlin.math.pow
+
+fun main() {
+
+//    var x = 15
+//    do {
+//        println(x)
+//        x++
+//    } while (x <= 35)
+//    println(x)
+
+
+//    for(i in 15 downTo 1 step 3){
+//       println(i)
+//    }
+//
+//    for(x in 1.until(10).step(3)){
+//        println(x)
+//    }
+
+
+//    var season = 3
+//    when(season){
+//        1 -> println("It's Spring")
+//        2 -> println("It's Summer")
+//        3 -> {
+//            println("It's Fall")
+//        }
+//        else -> println("Invalid Season")
+//    }
+
+
+//    var month = 3
+//    when(month){
+//        in 3..5 -> println("Spring")
+//        in 12, 1, 2 -> println("Winter")
+//    }
+//    println(total(6.0,5.0))
+
+    var name: String = "Daniel"
+//    name = null
+    var nullableName: String? = "Daniel"
+//    nullableName = null
+
+
+    //if let equivalent
+
+    nullableName?.let { println(it.length) }
+}
+
+fun myFunction() {
+    println("Called from myFunction")
+}
+
+
+fun total(x: Double, y: Double): Double {
+    return x.pow(y)
+}
