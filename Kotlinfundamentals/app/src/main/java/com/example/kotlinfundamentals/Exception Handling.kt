c3bc90de27cd11410f0f8d77c98a95f9d17a882f
Daniel <63873134+Zoom30@users.noreply.github.com>
2022-03-19 10:51:33 +0000

    initial commit

---
+++ Kotlinfundamentals/app/src/main/java/com/example/kotlinfundamentals/Exception Handling.kt
@@ -0,0 +1,5 @@
+package com.example.kotlinfundamentals
+
+fun main() {
+    println("Hello world")
+}
--- Kotlinfundamentals/app/src/main/java/com/example/kotlinfundamentals/Lambda Expression.kt
+++ Kotlinfundamentals/app/src/main/java/com/example/kotlinfundamentals/Lambda Expression.kt
@@ -1,2 +1,6 @@
 package com.example.kotlinfundamentals
 
+fun main() {
+    val sum: (Int, Int) -> Int = {a: Int, b: Int -> a + b}
+    val sumShorter = {a: Int, b: Int -> println(a + b)}
+}
