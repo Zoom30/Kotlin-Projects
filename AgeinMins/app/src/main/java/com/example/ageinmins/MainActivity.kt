dd5e6bf937a51800604c4dda53b1d3e26685c4e5
Daniel <63873134+Zoom30@users.noreply.github.com>
2022-03-22 00:08:20 +0000

    add the ability to select date

--- AgeinMins/app/src/main/java/com/example/ageinmins/MainActivity.kt
+++ AgeinMins/app/src/main/java/com/example/ageinmins/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.ageinmins
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var selectedDateTextView: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker: Button = findViewById<Button>(R.id.calculateButton)
+        selectedDateTextView = findViewById<TextView>(R.id.dateText)
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker(){
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{
+                    view, year, month, dateOfMonth ->
+                val selectedDate = "$dateOfMonth/$month/$year"
+                selectedDateTextView?.text = selectedDate
+
+
+
+            }, year, month, day).show()
     }
 }
