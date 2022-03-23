b0ed3b68a1bc68c6d37658645e57deae02614b15
Daniel <63873134+Zoom30@users.noreply.github.com>
2022-03-23 01:06:28 +0000

    adds calculator layout

--- CalculatorApp/androidApp/src/main/java/com/example/calculatorapp/android/MainActivity.kt
+++ CalculatorApp/androidApp/src/main/java/com/example/calculatorapp/android/MainActivity.kt
@@ -2,8 +2,10 @@ package com.example.calculatorapp.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.calculatorapp.Greeting
 import android.widget.TextView
+import android.widget.Toast
 
 fun greet(): String {
     return Greeting().greeting()
@@ -14,7 +16,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+    }
+
+    fun onDigit(view: View) {
+        Toast.makeText(this, "Button Clicked", Toast.LENGTH_LONG).show()
     }
 }
--- CalculatorApp/iosApp/iosApp/ViewController.swift
+++ CalculatorApp/iosApp/iosApp/ViewController.swift
@@ -10,10 +10,16 @@ import shared
 class ViewController: UIViewController {
     let greet = Greeting().greeting()
 
+    @IBOutlet var buttonsRow4: [UIButton]!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print(greet)
+        let x = buttonsRow4[2].titleLabel?.text
+        if let bla = x {
+            print("Printing : \(bla)")
+
+        }
     }
 
 
